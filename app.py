2986e67dcf716730f28b1ed80183b557994c55eb
PremSakshi <72404995+PremSakshi@users.noreply.github.com>
2021-05-13 05:49:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,18 +19,6 @@ def index():
     ##return "hello world"
 
 
-def shutdown_server():
-    func = request.environ.get('werkzeug.server.shutdown')
-    if func is None:
-        raise RuntimeError('Not running with the Werkzeug Server')
-    func()
-    
-@app.route('/shutdown', methods=['GET'])
-def shutdown():
-    shutdown_server()
-    return 'Server shutting down...'
-
-
 if __name__ == "__main__":
     app.run(debug=True)
 
